eeed6166886dba6f7f32f2fca30408bc8e4d95b4
Abhishek47580 <abhisheksharma47580@gmail.com>
2025-09-24 17:53:46 +0530

    Delete Attandance_management directory

--- Attandance_management/dayspassed.py
+++
@@ -1,8 +0,0 @@
-from datetime import date,timedelta
-
-def total_days_passed(a):
-    # satrting_date=date(a)
-    today_date=date.today()
-    days_passed=today_date-a
-    return days_passed.days
-tru=date(2053,3,5)
